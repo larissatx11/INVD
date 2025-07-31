76b145201c6c2f716b79676f089785620b1b235e
Raynara <raynaramartins.1999@gmail.com>
2025-07-31 00:05:22 -0300

    Refactor CSV processing script to include error handling and continuous monitoring for input file. Update input/output paths and enhance MinIO upload functionality. Minor adjustment in docker-compose.yml for volume formatting.

--- datalake/processing_csv/transform_csv.py
+++ datalake/processing_csv/transform_csv.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import os
+import time
 from minio import Minio
 from minio.error import S3Error
 
-INPUT_PATH = "/app/entrada_csv/saida_csv.csv"
-OUTPUT_PATH = "/app/saida_csv/saida_csv_tratado.csv"
+INPUT_PATH = "/app/ingest_csv/saida/saida_csv.csv"
+OUTPUT_PATH = "/app/processing_csv/saida_csv_tratado.csv"
 
 MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "minio:9000")
 ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
@@ -24,35 +25,43 @@ def transformar_csv(df):
     return df
 
 def upload_to_minio():
-    client = Minio(
-        MINIO_ENDPOINT,
-        access_key=ACCESS_KEY,
-        secret_key=SECRET_KEY,
-        secure=False
-    )
+    try:
+        client = Minio(
+            MINIO_ENDPOINT,
+            access_key=ACCESS_KEY,
+            secret_key=SECRET_KEY,
+            secure=False
+        )
 
-    if not client.bucket_exists(BUCKET_NAME):
-        client.make_bucket(BUCKET_NAME)
+        if not client.bucket_exists(BUCKET_NAME):
+            client.make_bucket(BUCKET_NAME)
 
-    client.fput_object(
-        bucket_name=BUCKET_NAME,
-        object_name=S3_OBJECT_NAME,
-        file_path=OUTPUT_PATH,
-        content_type="text/csv"
-    )
-    print(f"[MinIO] CSV enriquecido enviado para {BUCKET_NAME}/{S3_OBJECT_NAME}")
+        client.fput_object(
+            bucket_name=BUCKET_NAME,
+            object_name=S3_OBJECT_NAME,
+            file_path=OUTPUT_PATH,
+            content_type="text/csv"
+        )
+        print("[MinIO] CSV enriquecido enviado para:", f"{BUCKET_NAME}/{S3_OBJECT_NAME}")
+    except Exception as e:
+        print("[MinIO ERRO]", e)
 
 def main():
-    if not os.path.isfile(INPUT_PATH):
-        print(f"[ERRO] Arquivo {INPUT_PATH} não encontrado.")
-        return
+    while True:
+        if not os.path.isfile(INPUT_PATH):
+            print(f"[ERRO] Arquivo {INPUT_PATH} não encontrado.")
+        else:
+            try:
+                df = pd.read_csv(INPUT_PATH)
+                df = transformar_csv(df)
+                df.to_csv(OUTPUT_PATH, index=False)
+                print("[Transformação] CSV atualizado com sucesso.")
 
-    df = pd.read_csv(INPUT_PATH)
-    df = transformar_csv(df)
-    df.to_csv(OUTPUT_PATH, index=False)
-    print("[Transformação] CSV transformado salvo em:", OUTPUT_PATH)
+                upload_to_minio()
+            except Exception as e:
+                print("[ERRO PROCESSAMENTO]:", e)
 
-    upload_to_minio()
+        time.sleep(2)
 
 if __name__ == "__main__":
     main()
